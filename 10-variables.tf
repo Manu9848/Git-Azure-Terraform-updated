58d17586ec843fb00d11eeafcfd06a31331f1029
Manu8494 <manoharkalikiri1@email.com>
2023-10-07 16:22:47 +0530

    Upated subnets

--- 10-variables.tf
+++ 10-variables.tf
@@ -3,4 +3,8 @@ variable "rg_location" {}
 variable "costcenter" {}
 variable "env" {}
 variable "vnet_cidr" {}
+variable "vnet_cidr_webServers" {}
+variable "vnet_cidr_appservers" {}
+variable "vnet_cidr_dbservers" {}
 variable "kv_name" {}
+variable "AzureFirewallSubnet" {}
