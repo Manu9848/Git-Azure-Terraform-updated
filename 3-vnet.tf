aaaf2420429ebc7fee6ce8047091661c4026f0f5
Manu8494 <manoharkalikiri1@email.com>
2023-10-07 16:37:19 +0530

    Upated subnets

--- 3-vnet.tf
+++ 3-vnet.tf
@@ -8,27 +8,27 @@ resource "azurerm_virtual_network" "vnet1" {
 resource "azurerm_subnet" "web-servers" {
   name                 = "${var.rg_name}-vnet1-webservers"
   resource_group_name  = azurerm_resource_group.app10rg.name
-  virtual_network_name = azurerm_virtual_network.app10rg.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
   address_prefixes     = [var.vnet_cidr_webServers]
 }
 
 resource "azurerm_subnet" "app-servers" {
   name                 = "${var.rg_name}-vnet1-appservers"
   resource_group_name  = azurerm_resource_group.app10rg.name
-  virtual_network_name = azurerm_virtual_network.app10rg.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
   address_prefixes     = [var.vnet_cidr_appServers]
 }
 
 resource "azurerm_subnet" "db-servers" {
   name                 = "${var.rg_name}-vnet1-dbservers"
   resource_group_name  = azurerm_resource_group.app10rg.name
-  virtual_network_name = azurerm_virtual_network.app10rg.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
   address_prefixes     = [var.vnet_cidr_dbServers]
 }
 
 resource "azurerm_subnet" "fwsubnet" {
   name                 = "AzureFirewallSubnet"
   resource_group_name  = azurerm_resource_group.app10rg.name
-  virtual_network_name = azurerm_virtual_network.app10rg.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
   address_prefixes     = [var.AzureFirewallSubnet]
 }
