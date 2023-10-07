c87c0d2eb8ce0d0d96b21ca32dcebcaa8b7f4fee
Manu8494 <manoharkalikiri1@email.com>
2023-10-07 16:42:51 +0530

    Upated subnets

--- 10-variables.tf
+++ 10-variables.tf
@@ -3,7 +3,7 @@ variable "rg_location" {}
 variable "costcenter" {}
 variable "env" {}
 variable "vnet_cidr" {}
-variable "vnet_cidr_webServers" {}
+variable "vnet_cidr_webservers" {}
 variable "vnet_cidr_appservers" {}
 variable "vnet_cidr_dbservers" {}
 variable "kv_name" {}
