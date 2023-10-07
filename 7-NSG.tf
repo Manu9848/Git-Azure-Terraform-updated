858a948d87a65face6136918f2066ef8332d822c
Manu8494 <manoharkalikiri1@email.com>
2023-10-07 07:47:35 +0530

    added NSG security group

---
+++ 7-NSG.tf
@@ -0,0 +1,9 @@
+resource "azurerm_network_security_group" "webservers-nsg" {
+    name                = "webservers-nsg"
+    location            = azurerm_resource_group.app10rg.location
+    resource_group_name = azurerm_resource_group.app10rg.name
+    tags = {
+    CostCenter  = var.costcenter
+    Environment = var.env
+  }
+}
